5976a6d9fa89c80b8976f0f492466c2a39a25d30
prognroller <shvetsov1988@gmail.com>
2021-01-24 17:31:09 +0200

    refactoring links and adding svg sprite

--- source/js/script.js
+++ source/js/script.js
@@ -3,3 +3,11 @@ let elements = document.querySelectorAll('[class*=--no-js]')
 for (let el of elements) {
   el.classList.forEach(className => className.includes('--no-js') && el.classList.remove(className) )
 }
+
+let toggle = document.querySelector('#menu-toggle')
+let header = document.querySelector('#header')
+
+toggle.addEventListener(
+  'click',
+  (e) => { header.classList.toggle('header--opened') }
+)
