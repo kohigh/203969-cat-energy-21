78e64bdd140dfb6cd3745053fc2b5675783bcb64
prognroller <shvetsov1988@gmail.com>
2021-01-25 20:43:18 +0200

    range new 3

--- source/js/script.js
+++ source/js/script.js
@@ -19,8 +19,44 @@ let imgAfter = document.querySelector('.range__img--after')
 range.addEventListener(
   'change',
   (e) => {
-    imgBefore.style.width = `${e.target.value}%`
-    imgAfter.style.width = `${100 - e.target.value}%`
-    imgAfter.style.left = `${e.target.value}%`
+    console.log(e.target.value)
+    imgBefore.style.display = 'block'
+    imgAfter.style.display = 'block'
+
+    imgAfter.style.width = `${e.target.value}%`
+    imgBefore.style.width = imgAfter.style.left = `${100 - e.target.value}%`
+  }
+)
+
+let rangeBefore = document.querySelector('#range-before')
+let rangeAfter = document.querySelector('#range-after')
+let rangeProgress = document.querySelector('.range__progress')
+
+rangeBefore.addEventListener(
+  'click',
+  (e) => {
+    imgBefore.style.display = 'block'
+    imgBefore.style.width = '100%'
+
+    imgAfter.style.display = 'none'
+    imgAfter.style.left = '50%'
+
+    rangeProgress.classList.remove('range__progress--after')
+    range.value = 0
+  }
+)
+
+rangeAfter.addEventListener(
+  'click',
+  (e) => {
+    imgBefore.style.display = 'none'
+    imgBefore.style.width = '50%'
+
+    imgAfter.style.display = 'block'
+    imgAfter.style.left = '0%'
+    imgAfter.style.width = '100%'
+
+    rangeProgress.classList.add('range__progress--after')
+    range.value = 100
   }
 )
